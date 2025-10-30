529cba82f6fafa9090b90938d1534aa35759cc42
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2025-10-30 17:57:40 +0200

    feat: add footer

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import Header from "./sections/Header";
 import "./Landing.css";
+import Footer from "./sections/Footer";
 
 export default function Landing(){
     return(
         <div>
             <Header />
+            <Footer/>
         </div>
     )
 }
