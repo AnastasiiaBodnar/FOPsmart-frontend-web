7c7b48f8fffc03e1fd7300e0844defa8a8859b4d
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2025-10-30 18:41:12 +0200

    feat: add hero

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,13 @@
 import Header from "./sections/Header";
 import "./Landing.css";
 import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
 
 export default function Landing(){
     return(
         <div>
             <Header />
+            <Hero />
             <Footer/>
         </div>
     )
