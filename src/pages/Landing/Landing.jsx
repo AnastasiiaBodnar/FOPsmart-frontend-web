fcfa2553834cb29ee643c2596ee01f1dcf055a40
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2025-11-09 00:45:10 +0200

    feat(landing): add section how it works

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,13 +1,14 @@
 import Header from "./sections/Header";
-import "./Landing.css";
 import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
+import How from "./sections/How";
 
 export default function Landing(){
     return(
         <div>
             <Header />
             <Hero />
+            <How />
             <Footer/>
         </div>
     )
